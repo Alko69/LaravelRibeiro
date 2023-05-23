eebc0cf07c0c8e718bf636ec418ab5324dad104d
Cedric <c.louchard@it-students.fr>
2023-05-23 15:06:57 +0200

    commit 2

---
+++ database/migrations/2023_05_23_120443_init_table.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class init_table extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->float('price');
+            $table->integer('quantity');
+            $table->string('description');
+            $table->timestamps();
+        });
+
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+
+            $table->BigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->id();
+            $table->integer('quantity');
+            $table->string('product_price');
+
+
+            $table->BigInteger('order_id');
+            $table->foreign('order_id')->references('id')->on('orders');
+
+            $table->BigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_product');
+        Schema::dropIfExists('orders');
+        Schema::dropIfExists('products');
+    }
+}
