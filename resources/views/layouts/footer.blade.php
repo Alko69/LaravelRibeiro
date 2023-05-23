5a55cba2b5057608b64697eed2c2af1164c5aa5b
Joory71 <79840561+Joory71@users.noreply.github.com>
2023-05-23 17:15:47 +0200

    ajout medias

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,11 +3,11 @@
         <div class="row">
             <div class="col-lg-4 text-center">menu footer</div>
             <div class="col-lg-8 text-center">
-                map google
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2783.04510925!2d4.8612177766429365!3d45.770286212963626!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4ea85718bc503%3A0xd8b25d38030c2!2sIT-Akademy!5e0!3m2!1sfr!2sfr!4v1684854312738!5m2!1sfr!2sfr" width="600" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
         <div class="row text-center">
-            <p>Copiright 2023</p>
+            <p class="text-center mt-3">© 2023 - Tous droits réservé<s></s></p>
         </div>
     </div>
 </footer>
