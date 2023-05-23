a57e26a390bc4f3700fa6d2873ecabdafbda1e28
alex vigne <a.vigne@it-students.fr>
2023-05-23 16:51:13 +0200

    maj

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,7 +9,6 @@
     <title>@yield('title')</title>
     @include('layouts.head')
 </head>
-<<<<<<< HEAD
 <header>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
@@ -39,28 +38,3 @@
         </div>
     </nav>
 </header>
-=======
-
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/">
-                        <img src="https://media.tenor.com/H-MJKsxfTe4AAAAd/erwin-erwin-smith.gif" width="30" height="30" alt="img logo projet">
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/helloworld">Helloworld</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Products</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-</html>
