a09db1636204d63eea374d7d608fbc729685bce5
Joory71 <79840561+Joory71@users.noreply.github.com>
2023-05-23 17:07:55 +0200

    ajout images

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,17 +2,19 @@
 Accueil
 @endsection
 @include('layouts.header')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <body class="antialiased">
     <div class="container products-wrapper">
         <div class="row">
+            <h2 class="text-center">Nos nouveaux produits</h2>
             <div class="col-12 col-lg-4 text-center">
-                One of three columns
+                <img src="/medias/alternateur.jpg" alt="alternateur">
             </div>
             <div class="col-12 col-lg-4 text-center">
-                One of three columns
+                <img src="/medias/bougie.jpg" alt="Bougie">
             </div>
             <div class="col-12 col-lg-4 text-center">
-                One of three columns
+                <img src="/medias/villebrequin.jpg" alt="Villebrequin">
             </div>
         </div>
         <div class="row mt-3">
@@ -25,12 +27,11 @@ Accueil
             </div>
         </div>
         <div class="row">
-            <div class="col-4 text-center"><img src="Instagram" alt="Instagram"></div>
-            <div class="col-4 text-center"><img src="Youtbue" alt="Youtube"></div>
-            <div class="col-4 text-center"><img src="Twitter" alt="Twitter"></div>
+            <div class="col-4 text-center"><i class="fa-brands fa-facebook"></i></div>
+            <div class="col-4 text-center"><i class="fa-brands fa-youtube"></i></div>
+            <div class="col-4 text-center"><i class="fa-brands fa-twitter"></i></div>
         </div>
         
     </div>
 </body>
-
 @include('layouts.footer')
