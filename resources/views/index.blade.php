46d8e845cefa2b86ee13d01d9379a3de5b0a2cc8
Joory71 <79840561+Joory71@users.noreply.github.com>
2023-05-23 15:25:31 +0200

    ajout head.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,19 +7,23 @@
             <title>PBC</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-    </head>
+        @include('layouts.head')
+        </head>
     <body class="antialiased">
     @include('layouts.header')
-        <div class="container d-flex">
-            <div class="row d-flex justify-content-center">
-                <div class="col-4">test</div>
-                <div class="col-4">test</div>
-                <div class="col-4">test</div>
-            </div>
+    <div class="container">
+        <div class="row">
+          <div class="col-sm">
+            One of three columns
+          </div>
+          <div class="col-sm">
+            One of three columns
+          </div>
+          <div class="col-sm">
+            One of three columns
+          </div>
         </div>
+      </div>
     </body>
 </html>
 
