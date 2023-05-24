5c79f38cc7ad9fe9273da076a9b3e1a5b9d983c0
alex vigne <a.vigne@it-students.fr>
2023-05-24 15:40:10 +0200

    dossier user

---
+++ resources/views/users/listUsers.blade.php
@@ -0,0 +1,47 @@
+@section('title')
+Produits
+@endsection
+@include('layouts.header')
+
+<div class="container mt-2">
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-right mb-2">
+                <a class="btn btn-success" href="/user/add"> Add a user</a>
+            </div>
+        </div>
+    </div>
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+    <table class="table table-bordered">
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>password</th>
+            <th width="280px">Action</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach ($products as $product)
+            <tr>
+                <td>{{ $user->id }}</td>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+                <td>{{ $user->password }}</td>
+                    <form action="" method="Post">
+                        <a class="btn btn-primary" href="">Edit</a>
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
+@include('layouts.footer')
